6c902e1861cfaa36bf359a0a011ed1b67bdc4dc8
Jannis <jannis.wunsch@arvato-scs.com>
2022-08-26 12:13:34 +0200

    Movement

--- src/logic.ts
+++ src/logic.ts
@@ -245,9 +245,7 @@ export function move(gameState: GameState): MoveResponse {
                 possibleMoves.left = false
                 possibleMoves.up = false
                 possibleMoves.down = false
-            } 
-
-            if (mybody.some(Coord => Coord.x === (myHead.x + 1) && Coord.y === myHead.y) ){
+            } else if (mybody.some(Coord => Coord.x === (myHead.x + 1) && Coord.y === myHead.y) ){
                 possibleMoves.right = false 
             } else if (mybody.some(Coord => Coord.x === (myHead.x - 1) && Coord.y === myHead.y)){
                 possibleMoves.left = false
