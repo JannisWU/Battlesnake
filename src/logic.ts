36afafa338f8a02cb796c398a7badc1ab5b33ba5
Jannis <jannis.wunsch@arvato-scs.com>
2022-08-26 12:53:10 +0200

    Movement

--- src/logic.ts
+++ src/logic.ts
@@ -55,6 +55,7 @@ export function move(gameState: GameState): MoveResponse {
     const minWidth = 0
     const minHeight = 0
 
+    
     if (myHead.x === (boardWidth - 1)){
         possibleMoves.right = false
     }
@@ -223,7 +224,37 @@ export function move(gameState: GameState): MoveResponse {
             if (mybody.some(Coord => Coord.x === myHead.x && Coord.y === (myHead.y + 1))){
                 possibleMoves.up = false
             }
-            
+
+            if (myHead.x === (boardWidth - 1)){
+                possibleMoves.right = false
+            }
+            if (myHead.y === (boardHeight - 1)){
+                possibleMoves.up = false
+            }
+            if (myHead.x === minWidth){
+                possibleMoves.left = false
+            }
+            if (myHead.y === minHeight){
+                possibleMoves.down = false
+            }
+        
+            if (myHead.y === 0 && myHead.x === 0){
+                possibleMoves.down = false
+                possibleMoves.left = false 
+            }
+            if (myHead.y === boardHeight - 1 && myHead.x === 0){
+                possibleMoves.left = false
+                possibleMoves.up = false
+            }
+            if (myHead.y === boardHeight - 1 && myHead.x === boardWidth - 1){
+                possibleMoves.right = false
+                possibleMoves.up = false
+            }
+            if (myHead.y === 0 && myHead.x === boardWidth - 1){
+                possibleMoves.down = false
+                possibleMoves.right = false
+            }
+
             if (myHead.x === (boardWidth - 1) && myHead.y === (boardHeight - 1)) {
                 possibleMoves.down = false
                 possibleMoves.up = false
@@ -257,6 +288,7 @@ export function move(gameState: GameState): MoveResponse {
                 possibleMoves.up = false
                 possibleMoves.down = false
             }  
+
             
         }
 
