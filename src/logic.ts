4fa6acf185e87e59a9d9441e4bb720923352014f
Jannis <jannis.wunsch@arvato-scs.com>
2022-08-30 10:45:36 +0200

    SpiralenTest

--- src/logic.ts
+++ src/logic.ts
@@ -290,7 +290,8 @@ export function move(gameState: GameState): MoveResponse {
                 possibleMoves.left = false
                 possibleMoves.up = false
                 possibleMoves.down = false
-            } else if (mybody.some(Coord => Coord.x === (myHead.x + 1) && Coord.y === myHead.y)){
+            }
+            if (mybody.some(Coord => Coord.x === (myHead.x + 1) && Coord.y === myHead.y)){
                 possibleMoves.left = false 
                 possibleMoves.right = false
                 possibleMoves.down = false
